a23de4a8e184275664cbe17ecb59c1158ca9ee94
Silvanafadilla <sisilfadilla15@gmail.com>
2020-01-27 15:58:55 +0700

    Membuat factory dan tinker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(PostsTableSeeder::class);
-        $this->call(SiswaTableSeeder::class);
-        $this->call(BiodataTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
+        // $this->call(PostsTableSeeder::class);
+        // $this->call(SiswaTableSeeder::class);
+        // $this->call(BiodataTableSeeder::class);
+        // // $this->call(UsersTableSeeder::class);
+        factory(App\Tabungan::class, 100)->create();
+        factory(App\Customer::class, 1000)->create();
     }
 }
